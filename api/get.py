c110c471ee670458b9e82a052b632805af7c9646
minpeter <kali2005611@gmail.com>
2022-03-20 19:35:27 +0900

    rm get_code and rp get_problem_solving

--- api/get.py
+++ api/get.py
@@ -38,24 +38,3 @@ def get_problem_solving(session: requests.Session, solve_id: int) -> ProblemSolv
         "code_length": to_int(tr[8].text),
         "sub_time": to_int(tr[9][0].attrib['data-timestamp'])
     }
-
-# 성공시 코드 및 문제 번호, 실패시 None 반환
-def get_code(session: requests.Session, source_id: int) -> str:
-    res = session.get(f"https://www.acmicpc.net/source/{source_id}",
-        headers = make_header(),
-        cookies = make_cookies()
-    )
-    if res.status_code == 200:
-        print("status_code: 200")
-        html = res.text
-        soup = BeautifulSoup(html, 'html.parser')
-
-        if soup.find("span", "result-ac"):
-            code = soup.select_one("body > div.wrapper > div.container.content > div.row > div:nth-child(2) > textarea")
-            problem_id = soup.select_one("body > div.wrapper > div.container.content > div.row > div:nth-child(8) > div > table > tbody > tr > td:nth-child(3) > a")
-            lang = soup.select_one("body > div.wrapper > div.container.content > div.row > div:nth-child(8) > div > table > tbody > tr > td:nth-child(8)")
-            return {"code": code.text, "problem_id": problem_id.text, "lang": lang.text}
-    else:
-        print(f"status code: {res.status_code}")
-
-    return None
--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from dotenv import load_dotenv
-from api.get import get_code, get_problem_solving
+from api.get import get_problem_solving
 from api.lang import lang_to_code
 from api.submit import solving_submit
 import requests
 
 load_dotenv(verbose=True)
 # auth는 load_dotenv 아래에 있어야함.
-
-<<<<<<< HEAD
 session = requests.Session()
 
 for i in range(13000000, 350000, -1):
-  result = get_code(session, i)
+  result = get_problem_solving(session, i)
   if result != None:
     if solving_submit(session, result["problem_id"], 'open', result["code"], result["lang"]):
       print("🚀 성공")
@@ -20,22 +18,5 @@ for i in range(13000000, 350000, -1):
       print("❗ 실패")
   else:
     print("❗ 실패")
-=======
-
-session = requests.Session()
-
-print(get_problem_solving(session, 17280484))
-
-# result = get_code(session, 16270141)
-# if result != None:
-
-#   if solving_submit(session, result["problem_id"], 'open', result["code"], lang_to_code(result["lang"])):
-#     print("🚀 성공")
-#     print(f"https://www.acmicpc.net/status?problem_id={result['problem_id']}")
-#   else:
-#     print("❗ 실패")
-# else:
-#   print("❗ 실패")
->>>>>>> 43f302063bd43e06e15a218155e4db71f35ae3a3
 
 session.close()

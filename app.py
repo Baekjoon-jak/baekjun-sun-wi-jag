91e41443391485890e4351bc1f942e5ba3e0f307
minpeter <kali2005611@gmail.com>
2022-03-20 19:25:16 +0900

    add lang list

--- app.py
+++ app.py
@@ -6,19 +6,17 @@ import requests
 load_dotenv(verbose=True)
 # auth는 load_dotenv 아래에 있어야함.
 
-from auth import make_cookies, make_header
-
 session = requests.Session()
 
-result = get_code(session, 16270141)
-if result != None:
-
-  if solving_submit(session, result["problem_id"], 'open', result["code"], result["lang"]):
-    print("🚀 성공")
-    print(f"https://www.acmicpc.net/status?problem_id={result['problem_id']}")
+for i in range(13000000, 350000, -1):
+  result = get_code(session, i)
+  if result != None:
+    if solving_submit(session, result["problem_id"], 'open', result["code"], result["lang"]):
+      print("🚀 성공")
+      print(f"https://www.acmicpc.net/status?problem_id={result['problem_id']}")
+    else:
+      print("❗ 실패")
   else:
     print("❗ 실패")
-else:
-  print("❗ 실패")
 
 session.close()

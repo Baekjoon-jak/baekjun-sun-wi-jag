75a6af13eddc0c538b8feea760ff311d0a24dfd1
Soju06 <34199905+Soju06@users.noreply.github.com>
2022-03-20 19:45:57 +0900

    오류 수우정

--- utils.py
+++ utils.py
@@ -2,11 +2,11 @@
 def to_int(st: str, default: int or None = None) -> int or None:
     try:
         return int(st)
-    except ValueError:
+    except:
         return default
 
 def try_get(a, i, default = None) -> any or None:
     try:
         return a[i]
-    except ValueError:
+    except:
         return default
